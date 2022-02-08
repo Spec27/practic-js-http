0fb6e1010041f9df3b7440682ceb64ff0ca312fb
Spec27 <shkvara27@gmail.com>
2022-02-08 22:47:20 +0200

    pocemon

--- src/js/01-pokemon.js
+++ src/js/01-pokemon.js
@@ -1,1 +1,27 @@
 import sampleTpl from '../templates/sample.hbs';
+import '../css/common.css';
+import API from './api-service';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+refs.searchForm.addEventListener('submit', oneSearch);
+
+function oneSearch(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const serrchQuery = form.elements.query.value;
+
+  API.fetcPokemon(serrchQuery)
+    .then(renderInterfaceCard)
+    .catch(onFetchEror)
+    .finally(() => form.reset());
+}
+
+function renderInterfaceCard(pokemon) {
+  const markup = sampleTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+function onFetchEror() {
+  alert('Упс братан Ошибочка получаеться');
+}
---
+++ src/js/api-service.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://pokeapi.co/api/v2';
+function fetcPokemon(pokemonId) {
+  const url = `${BASE_URL}/pokemon/${pokemonId}`;
+  return fetch(url).then(response => {
+    return response.json();
+  });
+}
+export default { fetcPokemon };
