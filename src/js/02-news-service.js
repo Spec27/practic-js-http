eda2ff1e3c90d316fedd2375632857de796fc2f4
Spec27 <shkvara27@gmail.com>
2022-02-10 21:02:09 +0200

    33333

--- src/js/02-news-service.js
+++ src/js/02-news-service.js
@@ -1,10 +1,17 @@
 /* const API_KEY = 'a7cff487e6274380843b0bbdb7ed9ea0'; */
-/* const BASE_URL = 'https://newsapi.org/v2'; */
+/* const BASE_URL = 'https://newsapi.org/v2';
 /* const options = {
   headers: {
     Authorization: API_KEY,
   },
 }; */
+const options = {
+  headers: {
+    Connection: upgrade,
+    Upgrade: HTTP / 2.0,
+  },
+};
+
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
@@ -14,6 +21,7 @@ export default class NewsApiService {
   fetchArticles() {
     return fetch(
       `https://newsapi.org/v2/everything?q=${this.searchQuery}&apiKey=a7cff487e6274380843b0bbdb7ed9ea0&pageSize=5&page=${this.page}`,
+      options,
     )
       .then(response => response.json())
       .then(({ articles }) => {
